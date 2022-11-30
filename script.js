87a414b16607a8f3a461e1c26b906f5561d577c8
1lkhvm <tu6wkkvyr@code.edu.az>
2022-12-01 00:19:02 +0400

    1,2 Complete 3 yoxladim alinmadi

---
+++ script.js
@@ -0,0 +1,75 @@
+// // Task-1
+// // let arr=[1,25,244,44,55,22,64]
+
+// // let edediorta=arr.reduce((a,b)=>(a+b)) / arr.length;
+
+// // document.write(edediorta);
+
+// // cavab:65 Isleyir
+// /////////////////////////////////////////////////////////////////
+
+// // Task-2
+
+// // 1-ci usul
+// // let arr=[1,5,2,7]
+
+// // let.splice(4,0,11)
+
+// // console.log(arr)
+
+
+
+
+// // 2-ci usul
+// // let arr=[1,5,2,7];
+
+// // arrnew=arr.push(11);
+
+// // console.log(arr);
+
+
+
+
+// // 3-cu usul
+
+// // let arr=[1,5,2,7];
+// // let arr2=[11];
+// // let newarr=arr.concat(arr2)
+
+// // console.log(newarr);
+
+
+
+// //  Task-3
+{
+n=0
+N=0
+let count = 0
+let arr =["NNNNNNN?"]
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i]== 'n' && arr[i]== 'N' ) {
+        count++
+    }
+    
+    
+console.log(arr)
+
+
+}
+}
+
+// alinmadi
+
+// //  Task-4
+
+
+    
+    
+
+
+
+
+
+
+
+    
